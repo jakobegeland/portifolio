025d3896841014bd253cbe82da3cf2332580c2df
Jakobegeland <jakob.f.e@hotmail.no>
2021-09-15 22:52:43 +0200

    forste test

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import In1060 from './pages/projects/In1060';
+import In2000 from './pages/projects/In2000';
+import In3010 from './pages/projects/In3010';
+import IfiNavet from './pages/projects/IfiNavet';
+import AjAj from './pages/projects/AjAj';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <div className="App">
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/IN1060" component={In1060} />
+      <Route path="/IN2000" component={In2000} />
+      <Route path="/IN3010" component={In3010} />
+      <Route path="/IfiNavet" component={IfiNavet} />
+      <Route path='*' component={AjAj} />
+    </Switch>
+    </div>
   </BrowserRouter>,
   document.getElementById('root')
 );
