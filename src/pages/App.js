a9ce923bff79516f3fbd06766df6b52287849ebc
Jakobegeland <jakob.f.e@hotmail.no>
2021-09-22 14:01:28 +0200

    j

--- src/pages/App.js
+++ src/pages/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
         <Switch>
-          <Route path={process.env.PUBLIC_URL + "/"} component={Home} />
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
           <Route path="/IN1060" component={In1060} />
           <Route path="/IN2000" component={In2000} />
           <Route path="/IN3010" component={In3010} />
